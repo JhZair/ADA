4af6809b748faf5c34d3cb08fb84980c54103ca1
JhZair <98540197+JhZair@users.noreply.github.com>
2026-03-18 18:07:23 -0500

    Add files via upload

---
+++ MetodosOrdenamiento/InsertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<class T>
+void Insertion(vector<T>& v){
+	for(int j = 1; j< v.size(); j++){
+		int key = v[j];
+		int i = j-1;
+		while(i>=0 && v[i] > key){
+			v[i+1] = v[i];
+			i -= 1;
+		}
+		v[i+1] = key;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	vector<int> vec = {5, 2, 4, 6, 1, 3};
+	Insertion<int>(vec);
+	for(auto v : vec){
+		cout << v << ' ';
+	}
+	return 0;
+}
+
---
+++ MetodosOrdenamiento/MergeSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Merge(vector<int>& v, int p, int q, int r)
+
+int main(int argc, char *argv[]) {
+	vector<int> vec = {5, 2, 4, 6, 1, 3};
+	Merge<int>(vec);
+	for(auto v : vec){
+		cout << v << ' ';
+	}
+	return 0;
+	return 0;
+}
+
