e66d83cc1437b95176500e4223d1b31554047934
JhZair <98540197+JhZair@users.noreply.github.com>
2026-05-20 18:07:03 -0500

    Add files via upload

---
+++ Lab_algs_voraces/Max_programas.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Programa{
+	int id;
+	int espacio;
+};
+bool compararPorEspacio(Programa a , Programa b) {
+	return a.espacio < b.espacio;
+}
+
+void maxProgramas(int capacidad, vector<Programa>& programas) {
+	sort(programas.begin(), programas.end(), compararPorEspacio);
+	int espacio_total=0;
+	int progs_almacenados = 0;
+	while(espacio_total + programas[1].espacio <= capacidad ) {
+		cout << "Se usa programa con id: " << programas[1].id<< endl;
+		espacio_total += programas[1].espacio;
+		progs_almacenados++;
+	}
+	cout << "Se usa: " << espacio_total <<" de espacio"<< endl;
+	cout << "Sobra: " << capacidad - espacio_total <<" de espacio"<< endl;
+	cout << "Se pueden almacenar: " << progs_almacenados<< " programas"<<endl;
+}
+
+int main(int argc, char *argv[]) {
+	vector<Programa> programas = {
+		{1 , 40} ,
+	{2 , 10} ,
+	{3 , 30} ,
+	{4 , 20} ,
+	{5 , 50}
+	};
+	int capacidadDisco = 100;
+	maxProgramas(capacidadDisco, programas);
+	return 0;
+}
+
---
+++ Lab_algs_voraces/Monedas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void cambio(int monto, vector<int>& monedas){
+	sort(monedas.begin(),monedas.end(),greater<int>());
+	int total_mone = 0;
+	
+	cout << " Monto a devolver : " << monto << endl ;
+	
+	for(int i=0;i<monedas.size();i++){
+		if(monto==0) break;
+		int valor_moneda = monedas[i];
+		if(monto >= valor_moneda){
+			cout << "Una moneda de: "<< valor_moneda <<" céntimos"<< endl ;
+			int cantidad = monto/valor_moneda;
+			monto = monto % valor_moneda;
+			total_mone += cantidad;
+		}
+	}
+	cout<< "Total de monedas: " << total_mone << endl;
+}
+
+int main(int argc, char *argv[]) {
+	
+	vector<int> monedas = {1, 2, 5, 10, 20, 50, 100};
+	int monto ;
+	cout << " Ingrese el monto en centimos : ";
+	cin >> monto;
+	cambio(monto, monedas);
+	return 0;
+}
+
